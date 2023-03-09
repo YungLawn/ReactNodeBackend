3d945d1c71f82d0b3c355ffd638939b6b252e6b6
YungLawnLaptop <ccteelon@gmail.com>
2023-03-09 14:15:56 -0500

    ok

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get("/api", (req, res) => {
-    res.json({"users": ["userOne", "usertwo", "userThree"]})
+    res.json({"users": ["userOne", "usertwo", "userThree", "userFour"]})
 })
 
 app.listen(5000, () => {console.log('server on port 5000') })
